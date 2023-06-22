dab1cebdd502890cbcf9bce32f8435190efaf854
bilel4449 <bileltrain44@gmail.com>
2023-06-22 11:23:40 +0200

    fin de projet

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-var btn=document.getElementsByName('ajouter');
-var zone=document.querySelectorAll('.zone');
-// var div=document.querySelector('.div')
+var btn=document.querySelector('.button');
+
+var id=document.querySelector('.zone');
+var nom=document.querySelector('#nomp');
+var prenom=document.getElementsByName('prenom');
+var telephone=document.getElementById('tel');
+
  btn.addEventListener('click',ajout);
+ function ajout(event){
+  var cible=btn.event
+  if(id.value==""){
+    alert("donner ID personnel");
+  }
+  else if(nom.value==""){
+    alert("donner nom personnel");
+   }
+   else if(prenom[0].value==""){
+    alert("donner prenom");
+   }
+   else if(telephone.value==""){
+    alert("donner le numero telephone");
+   }
+   var salaire=document.getElementById('select');
+   var v=salaire.value;
+   if(v=="choisir salaire"){
+   alert("choisir salaire")
+   }
+   else{
+    alert(nom.value+" "+"personnel ajoutée")
+   }
+  }
+ 
+  
+ 
 
- function ajout(event)
- {
-   var cible=event.target
 
-    if((cible.btn=="ajouter")&&(zone.innerHTML=="")){
-        alert("Svp saisie les informations de personnel")
-    }
-    else{
-        alert("personnel ajoutée")
-    }
- }
